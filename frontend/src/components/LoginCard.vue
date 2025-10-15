0f8eeedaf44d44abbbcb47202b8c3ebb4aa0378b
LauraKoNk <l.kokonyange@gmail.com>
2025-10-15 14:13:46 +0200

    Modif card login

--- frontend/src/components/LoginCard.vue
+++ frontend/src/components/LoginCard.vue
@@ -1,13 +1,14 @@
- <template>
-    <div class="flex flex-col items-center justify-center">
-      <form
-        class="text-card-text-color mt-10 w-full max-w-xs md:max-w-sm lg:max-w-md p-6 border border-general-border rounded-lg shadow"
-      >
-      <h1 class="text-xl text-center font-bold mb-4"> Connexion</h1>
-      </form>
+<template>
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="bg-sky-100 flex flex-col items-center justify-center border border-gray-500 rounded-lg shadow m-10 max-w-xs md:max-w-sm lg:max-w-md p-6 w-full">
+            <form class="text-card-text-color">
+                <h1 class="text-xl text-center font-bold mb-4"> Connexion</h1>
+            </form>
 
-      <div className="max-w-sm mx-auto p-6">
-        <p>Pas de compte ? <a href="#">S'inscrire</a></p>
-      </div>
+            <div className="max-w-sm mx-auto p-6">
+                <p>Pas de compte ? <a href="#">S'inscrire</a></p>
+            </div>
+        </div>
+
     </div>
- </template>
+</template>
