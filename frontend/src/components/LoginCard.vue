3ecdb1c87cd117581773e9743943c742470e73c1
Diangou <diangou.cmr09@gmail.com>
2025-10-15 14:29:13 +0200

    Page login

--- frontend/src/components/LoginCard.vue
+++ frontend/src/components/LoginCard.vue
@@ -4,13 +4,18 @@
             <form class="text-card-text-color">
                 <h1 class="text-xl text-center font-bold mb-4"> Connexion</h1>
             </form>
-
+            <LabelInput label="Email" placeholder="Entrez votre nom" v-model="email" />
+            <LabelInput label="Mot de passe" placeholder="Entrez votre email" v-model="email" />
+            <AppButton label="Connexion" @click="login" />
             <div className="max-w-sm mx-auto p-6">
                 <p>Pas de compte ? <a href="#">S'inscrire</a></p>
             </div>
         </div>
-
     </div>
 </template>
 
-<!--Card avec bg, Titre, les champs se créer un compte ...-->
+
+<script setup>
+import LabelInput from './LabelInput.vue';
+import AppButton from './Button.vue';
+</script>
