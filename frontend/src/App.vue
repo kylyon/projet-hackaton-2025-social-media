5a28531aab54e3a29344aac8537902c59905d988
LauraKoNk <l.kokonyange@gmail.com>
2025-10-15 13:20:39 +0200

    Ajout card de login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,10 @@
 <template>
   <main class="flex flex-col justify-center">
-     <checkBackendApi />
+    <LoginCard />
+     <Footer />
 
      <!--Intégration du Menu avec Headless UI -->
-  <Menu>
-    <MenuButton>Options</MenuButton>
-    <MenuItems>
-      <!-- Use the `active` state to conditionally style the active item. -->
-      <MenuItem
-        v-for="link in links"
-        :key="link.href"
-        as="template"
-        v-slot="{ active }"
-      >
-        <a
-          :href="link.href"
-          :class="{ 'bg-blue-500 text-white': active, 'bg-white text-black': !active }"
-        >
-          {{ link.label }}
-        </a>
-      </MenuItem>
-    </MenuItems>
-  </Menu>
+  
 
   </main>
 </template>
@@ -29,7 +12,9 @@
 <script setup>
 
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
-import checkBackendApi from './components/checkBackendApi.vue';
+import LoginCard from './components/LoginCard.vue';
+import Footer from './components/Footer.vue';
+import Login from './pages/Login.vue';
 
 
 const links = [
---
+++ frontend/src/components/LoginCard.vue
@@ -0,0 +1,13 @@
+ <template>
+    <div class="flex flex-col items-center justify-center">
+      <form
+        class="text-card-text-color mt-10 w-full max-w-xs md:max-w-sm lg:max-w-md p-6 border border-general-border rounded-lg shadow"
+      >
+      <h1 class="text-xl text-center font-bold mb-4"> Connexion</h1>
+      </form>
+
+      <div className="max-w-sm mx-auto p-6">
+        <p>Pas de compte ? <a href="#">S'inscrire</a></p>
+      </div>
+    </div>
+ </template>
