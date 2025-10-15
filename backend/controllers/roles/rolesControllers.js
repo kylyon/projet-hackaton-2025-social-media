24768bf28839638dcf5f7389f774b100fd6a6b57
elengadan@gmail.com <elengadan@gmail.com>
2025-10-15 13:49:02 +0200

    evol (controllers) inialisation des routes

--- backend/controllers/roles/rolesControllers.js
+++ backend/controllers/roles/rolesControllers.js
@@ -5,17 +5,20 @@ const getRoles = (req, res) => {
 
 // Create a new role
 const createRoles = (req, res) => {
-  res.status(200).json({ message: "Role create route is working", status: "200" });
+  const role = req.body;
+  res.status(200).json({ message: "Role create route is working", status: "200", role });
 };
 
 // Update role
 const updateRoles = (req, res) => {
-  res.status(200).json({ message: "Role update route is working", status: "200" });
+  const id = req.params.id;
+  res.status(200).json({ message: "Role update route is working", status: "200", id });
 };
 
 // Delete role
 const deleteRoles = (req, res) => {
-  res.status(200).json({ message: "Role delete route is working", status: "200" });
+  const id = req.params.id;
+  res.status(200).json({ message: "Role delete route is working", status: "200", id });
 };
 
 module.exports = {
---
+++ backend/routes/comments/comments.js
@@ -0,0 +1,58 @@
+const { Router } = require('express');
+const router = Router();
+
+const {
+  getComments,
+    createComment,
+    updateComment,
+    deleteComment } = require("../../controllers/comments/commentsControllers");
+
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Récupère la liste des commentaires
+ *     tags: [Comments]
+ *     responses:
+ *       200:
+ *         description: Liste des commentaires
+ */
+router.get('/', getComments);
+
+/**
+ * @swagger
+ * /comments/add:
+ *   post:
+ *     summary: Crée un nouveau commentaire
+ *     tags: [Comments]
+ *     responses:
+ *       200:
+ *         description: création du commentaire
+ */
+router.post('/add', createComment);
+
+/**
+ * @swagger
+ * /comments/update/1:
+ *   put:
+ *     summary: Met à jour un commentaire
+ *     tags: [Comments]
+ *     responses:
+ *       200:
+ *         description: commentaire mis à jour
+ */
+router.put('/update/:id', updateComment);
+
+/**
+ * @swagger
+ * /comments/delete/1:
+ *   delete:
+ *     summary: Supprime un commentaire
+ *     tags: [Comments]
+ *     responses:
+ *       200:
+ *         description: commentaire supprimé
+ */
+router.delete('/delete/:id', deleteComment);
+
+module.exports = router;
