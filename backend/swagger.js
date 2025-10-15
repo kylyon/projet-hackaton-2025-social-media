5db0ad434d265009aa083b77496839fc16b5bd59
elengadan@gmail.com <elengadan@gmail.com>
2025-10-15 09:27:48 +0200

    evol(swagger.js) add production link

--- backend/swagger.js
+++ backend/swagger.js
@@ -12,6 +12,9 @@ const swaggerDefinition = {
     {
       url: `http://localhost:${process.env.PORT || 3000}`,
     },
+    {
+      url: `https://hackaton-backend-api.vercel.app/`,
+    }
   ],
 };
 
