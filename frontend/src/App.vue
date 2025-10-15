aa38ec7dcce4980e1fda5ab202ec330620f39e03
Diangou <diangou.cmr09@gmail.com>
2025-10-15 14:47:19 +0200

    Formulaire connexion

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <main class="flex flex-col justify-center">
+    <LoginCard />
   </main>
 </template>
 
 <script setup>
+
+import LoginCard from './components/LoginCard.vue';
 
 </script>
