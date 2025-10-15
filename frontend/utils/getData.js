6bf2e6afa8d8c88eb7abe64fa4fa18016adca46c
elengadan@gmail.com <elengadan@gmail.com>
2025-10-15 16:41:12 +0200

    frontend: ajout utilitaires appel api

---
+++ frontend/utils/getData.js
@@ -0,0 +1,15 @@
+export const getData = async(url) => {
+    try {
+        const response = await fetch(`${url}`);
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json(); 
+        
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }
+
+};
