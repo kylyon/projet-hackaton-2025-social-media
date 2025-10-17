52ebc51bf44bdbc02d46b8e089e4be9e518f7089
LauraKoNk <l.kokonyange@gmail.com>
2025-10-17 12:36:45 +0200

    fix: supression elements head genants dans le code

--- frontend/src/components/PostCard.vue
+++ frontend/src/components/PostCard.vue
@@ -1,11 +1,13 @@
 <template>
-<<<<<<< HEAD
   <main class="min-h-screen flex justify-center">
-    <div class="bg-sky-100 border border-gray-300 rounded-lg shadow p-6 max-w-2xl w-full h-[90vh] overflow-hidden space-y-6">
-    <div class="bg-sky-100 border border-gray-300 rounded-lg shadow p-6 max-w-2xl w-full h-[90vh] overflow-hidden space-y-6">
-      <User :userId="userId" />
-      <div class="overflow-y-auto h-full pr-2">
-        <PostSection />
+    <div
+      class="bg-sky-100 border border-gray-300 rounded-lg shadow p-6 max-w-2xl w-full h-[90vh] overflow-hidden space-y-6">
+      <div
+        class="bg-sky-100 border border-gray-300 rounded-lg shadow p-6 max-w-2xl w-full h-[90vh] overflow-hidden space-y-6">
+        <User :userId="userId" />
+        <div class="overflow-y-auto h-full pr-2">
+          <PostSection />
+        </div>
       </div>
     </div>
   </main>
@@ -13,12 +15,7 @@
 
 <script setup>
 import User from '@/components/User.vue'
-<<<<<<< HEAD
 import PostSection from '@/components/PostSection.vue'
 
-=======
-
-// On passe l'ID de l'utilisateur (peut être dynamique selon ton post)
->>>>>>> e4d82af (Ajout de la section user dans la card des posts)
 const userId = 1
 </script>
