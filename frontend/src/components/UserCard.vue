2c022abacb7254eb1c83910483b04a25a8755566
LauraKoNk <l.kokonyange@gmail.com>
2025-10-17 11:27:59 +0200

    ajout modales modification user + hobbies

--- frontend/src/components/UserCard.vue
+++ frontend/src/components/UserCard.vue
@@ -1,7 +1,8 @@
 <template>
-     <div class="min-h-screen flex flex-col justify-center p-4">
-        <div class="elements flex flex-col items-center px-6 py-8 md:px-10 md:py-12 rounded-3xl gap-12 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg w-full mx-auto border border-gray-200 shadow-lg">
-            <div class="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 rounded-full overflow-hidden border-2">
+    <div class="min-h-screen flex flex-col justify-center p-4">
+        <div
+            class="elements bg-sky-100 border border-gray-300 flex flex-col items-center px-6 py-8 md:px-10 md:py-12 rounded-3xl gap-12 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg w-full mx-auto shadow-lg">
+            <div class="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 rounded-lg border border-gray-300 overflow-hidden">
                 <img src="#" alt="avatar" class="w-full h-full object-cover" />
             </div>
             <div class="user-infos text-center flex flex-col gap-2">
@@ -19,18 +20,31 @@
                     <p class="text-sm sm:text-sm md:text-base">hobbies</p>
                 </div>
                 <div class="description">
-                    <p class="text-xs sm:text-sm md:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officiis nobis minus optio amet nemo magnam, quae corrupti modi, sapiente dolore quod eos enim a sequi facere ipsa maxime asperiores!</p>
+                    <p class="text-xs sm:text-sm md:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quibusdam officiis nobis minus optio amet nemo magnam, quae corrupti modi, sapiente dolore quod
+                        eos enim a sequi facere ipsa maxime asperiores!</p>
                 </div>
             </div>
             <div class="user-components w-full space-y-4">
-                <AppButton label="Modifier mon profil" icon="UserIcon" variant="text" />
-                 <AppButton label="Modifier mes hobbies" icon="UserIcon" variant="text" />
+                <AppButton label="Modifier mon profil" icon="UserIcon" variant="text" @click="showModalUser = true" />
+                <AppButton label="Modifier mes hobbies" icon="UserIcon" variant="text" @click="showModalHobbies = true"/>
             </div>
         </div>
+        <!-- On affiche la modale seulement si showModal = true -->
+        <UpdateUserCard v-if="showModalUser" :user="user" @close="showModalUser = false" />
+        <!-- On affiche la modale seulement si showModal = true -->
+        <UpdateHobbieCard v-if="showModalHobbies" :user="user" @close="showModalHobbies = false" />
+
     </div>
 </template>
 
 <script setup>
 import AppButton from '@/components/Button.vue'
+import UpdateUserCard from '@/components/UpdateUserCard.vue'
+import UpdateHobbieCard from '@/components/UpdateHobbieCard.vue'
+import { ref } from 'vue'
+
+const showModalUser = ref(false);
+const showModalHobbies = ref(false)
 
 </script>
